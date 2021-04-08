5ef39b828214f5f918d382b5bd188816854c8cec
Alfred <alfred.b.wong@gmail.com>
2021-04-07 21:55:23 -0400

    Add code for signing in with email/gmail

--- app/src/main/java/com/udacity/project4/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/project4/login/LoginFragment.kt
@@ -1,10 +1,16 @@
 package com.udacity.project4.login
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentLoginBinding
@@ -18,9 +24,14 @@ class LoginFragment : BaseFragment() {
 
     private lateinit var binding: FragmentLoginBinding
 
+    companion object {
+        const val TAG = "LoginFragment"
+        const val SIGN_IN_REQUEST_CODE = 1001
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding.loginButton.setOnClickListener { launchSignInFlow() }
 
         return binding.root
     }
@@ -28,4 +39,36 @@ class LoginFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    private fun launchSignInFlow(){
+        val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_REQUEST_CODE
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                LoginFragment.SIGN_IN_REQUEST_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
 }
