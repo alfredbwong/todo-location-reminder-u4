9fbddbeffbbef3f2c1e9ba6bfb7ddf272af65ffe
Alfred <alfred.b.wong@gmail.com>
2021-04-07 22:13:45 -0400

    Update after login to go to reminder list fragment

--- app/src/main/java/com/udacity/project4/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/project4/login/LoginFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
@@ -63,6 +64,11 @@ class LoginFragment : BaseFragment() {
             if (resultCode == Activity.RESULT_OK) {
                 // User successfully signed in
                 Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+
+                //Redirect user to the Reminders list view
+                val navController = findNavController()
+                val action = LoginFragmentDirections.actionLoginFragmentToReminderListFragment()
+                navController.navigate(action)
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
